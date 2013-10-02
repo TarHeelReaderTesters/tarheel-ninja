ea97aeb653d5e1ef8cea15f8b953fb1883a7eb8e
Roxanne <roxestradah@gmail.com>
2013-10-01 23:04:49 -0400

    main menu testing script, still getting an error, maybe running too fast?

---
+++ mainMenu.py
@@ -0,0 +1,46 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
+
+#testing Main Menu
+
+class MainMenu(unittest.TestCase):
+
+        def setUp(self):
+                self.driver = webdriver.Chrome() # Get local session of firefox
+
+                
+        def test_main_menu(self):
+                driver = self.driver
+                driver.get("http://www.tarheelreader.org") # Load page
+                self.assertIn("Tar Heel Reader", driver.title)
+
+                
+                menuNames = ['Home', 'Find a book', 'Collections', 'Favorites', 'Write a book', 'Log in', 'Help', 'Your books', 'Announcements', 'Photo credits']
+                for menuName in menuNames:        # Second Example
+                    print 'Current name :', menuName
+                    
+                    try:
+                            well = driver.find_element_by_xpath("//img[contains(@src,'http://tarheelreader3s.cs.unc.edu/themeV1/images/well.png')]")# Find the main menu
+                            well.click()
+                            WebDriverWait(driver, 10).until(lambda s: s.find_element_by_class_name("navigationMenu").is_displayed())
+                    except NoSuchElementException:
+                            assert 0, "Cannot find main menu"
+                    try:
+                            homePage = driver.find_element_by_link_text(menuName)
+                            homePage.click()
+                            driver.back()
+                            
+                    except NoSuchElementException:
+                        assert 0, "Cannot find " + menuName + " button"
+
+             
+                        
+        def tearDown(self):
+                self.driver.close()
+             
+if __name__ == "__main__":
+        unittest.main()
