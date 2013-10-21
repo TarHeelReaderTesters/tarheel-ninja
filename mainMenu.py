cc722f4c09eb82250b1a5bb70dbe3e382aeea530
Roxanne <roxestradah@gmail.com>
2013-10-21 15:06:07 -0400

    main menu adjustments

--- mainMenu.py
+++ mainMenu.py
@@ -10,21 +10,21 @@ import time
 class MainMenu(unittest.TestCase):
 
         def setUp(self):
-                self.driver = webdriver.Chrome() # Get local session of firefox
+                self.driver = webdriver.Firefox() 
 
                 
         def test_main_menu(self):
                 driver = self.driver
-                driver.get("http://www.tarheelreader.org") # Load page
+                driver.get("http://www.tarheelreader.org")
                 self.assertIn("Tar Heel Reader", driver.title)
 
                 
                 menuNames = ['Home', 'Find a book', 'Collections', 'Favorites', 'Write a book', 'Log in', 'Help', 'Your books', 'Announcements', 'Photo credits']
-                for menuName in menuNames:        # Second Example
+                for menuName in menuNames:      
                     print 'Current name :', menuName
                     
                     try:
-                            well = driver.find_element_by_xpath("//img[contains(@src,'http://tarheelreader3s.cs.unc.edu/themeV1/images/well.png')]")# Find the main menu
+                            well = driver.find_element_by_xpath("//img[contains(@src,'http://tarheelreader3s.cs.unc.edu/themeV1/images/well.png')]")
                             well.click()
                             WebDriverWait(driver, 10).until(lambda s: s.find_element_by_class_name("navigationMenu").is_displayed())
                     except NoSuchElementException:
