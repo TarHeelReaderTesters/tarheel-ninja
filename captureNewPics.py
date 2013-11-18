20f9babef8c625a1aef0d61e5186eaabb374cd42
huynhl <huynhl@live.unc.edu>
2013-11-18 16:39:43 -0500

    added scripts for base and comparison screenshots

---
+++ captureNewPics.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import os
+import sys
+import time
+import unittest
+
+MAX_WAIT_TIME=30
+
+class screenshot(unittest.TestCase):
+    def setUp(self):
+        if(param == 3):
+            self._browser = webdriver.Remote(desired_capabilities = {"platform": param[1],"browserName": param[2], "version": param[3]})
+        else:
+            self._browser = webdriver.Remote(desired_capabilities = {"platform": param[1],"browserName": param[2]})
+                
+    def test_screenShot(self):
+        """Runs the book searching test for Tar Heel Reader
+		"""
+        self._browser.set_window_size(800,600)
+        self._browser.set_window_position(0,0)
+        #make a directory named 'images'
+        if not os.path.exists('images'):
+            os.makedirs('images')
+        if not os.path.exists('images/new/'):
+            os.makedirs('images/new/')
+        if not os.path.exists('images/new/' + param[1]):
+            os.makedirs('images/new/' + param[1])
+        if not os.path.exists('images/new/' + param[1] +'/' + param[2]):
+            os.makedirs('images/new/' + param[1] +'/' + param[2])
+
+        url = ['http://tarheelreader.org/2013/11/18/tarheelreadertestbook/', 'http://tarheelreader.org/2013/11/18/tarheelreadertestbook/2/', 'http://tarheelreader.org/2013/11/18/tarheelreadertestbook/3/', 'http://tarheelreader.org/2013/11/18/tarheelreadertestbook/4/', 'http://tarheelreader.org/2013/11/18/tarheelreadertestbook/5/']
+        
+        screenshot_name = ['title', 'page2', 'page3', 'page4', 'page5_japanese']
+        for i in range(5):
+            name = screenshot_name[i] + '.png'
+            self._browser.get(url[i])
+            time.sleep(3.0)
+            self._browser.save_screenshot('images/new/' + param[1] + '/' + param[2] +'/' + name)
+
+
+		#Load home page of Tar Heel Reader
+        #self._browser.get(param[3])
+        #self._browser.implicitly_wait(10)
+        #time.sleep(10.0)
+	#Load home page of Tar Heel Reader
+    #self._browser.get("http://tarheelreader.org/2013/10/22/pandas-can-eat/")
+        #self._browser.save_screenshot(name)
+        #self._browser.get_screenshot_as_file(name)
+        assert "Tar Heel Reader" in self._browser.title
+            
+    def tearDown(self):
+        time.sleep(5.0)
+        print '\n Ran ' + param[0]
+        print 'Platform: ' + param[1]
+        print 'Browser: ' + param[2]
+        self._browser.quit()
+        
+if __name__ == '__main__':
+    param = []
+    if len(sys.argv) == 4:
+        param.append(sys.argv[0])
+        param.append(sys.argv[1])
+        param.append(sys.argv[2])
+        param.append(sys.argv[3])
+        del sys.argv[1:]
+        del sys.argv[2:]
+        del sys.argv[3:]
+    else:
+        param.append(sys.argv[0])
+        param.append(sys.argv[1])
+        param.append(sys.argv[2])
+        del sys.argv[1:]
+        del sys.argv[2:]
+        del sys.argv[3:]
+
+    unittest.main()
+    
