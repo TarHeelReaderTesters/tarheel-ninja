c40dd15ad81f72a3120ee281d67f24e567cd5c04
Long <huynhl@live.unc.edu>
2013-12-07 21:51:03 -0500

    Added ULR param to textColor

--- textColor.py
+++ textColor.py
@@ -8,23 +8,23 @@ import sys
 
 class testTextColor(unittest.TestCase):
     def setUp(self):
-        if len(param) == 4:
-            self._browser = webdriver.Remote(desired_capabilities = {"platform": param[1],"browserName": param[2], "version": param[3]})
+        self.url = param[1]
+
+        if len(param) == 5:
+            self._browser = webdriver.Remote(desired_capabilities = {"platform": param[2],"browserName": param[3], "version": param[4]})
         else:
-            self._browser = webdriver.Remote(desired_capabilities = {"platform": param[1],"browserName": param[2]})
+            self._browser = webdriver.Remote(desired_capabilities = {"platform": param[2],"browserName": param[3]})
             
         self._browser.implicitly_wait(30)
-        self.base_url = "http://tarheelreader.org/"
         self.verificationErrors = []
         self.accept_next_alert = True
     
     def test_text_color(self):
-        browser = self._browser
-        browser.get(self.base_url + "2013/11/18/tarheelreadertestbook/")
-        browser.find_element_by_css_selector("img[alt=\"Settings\"]").click()
-        browser.find_element_by_css_selector("span.colors").click()
-        browser.find_element_by_css_selector("li.textColors > span").click()
-        browser.find_element_by_xpath("//li[2]/ul/li[7]/span").click()
+        self._browser.get(self.url + '/' + "2013/11/18/tarheelreadertestbook/")
+        self._browser.find_element_by_css_selector("img[alt=\"Settings\"]").click()
+        self._browser.find_element_by_css_selector("span.colors").click()
+        self._browser.find_element_by_css_selector("li.textColors > span").click()
+        self._browser.find_element_by_xpath("//li[2]/ul/li[7]/span").click()
 
         rgb = self._browser.find_element_by_xpath("//div[contains(@class,'thr-book-page')]").value_of_css_property('color')
            
@@ -37,32 +37,34 @@ class testTextColor(unittest.TestCase):
     
 
     def tearDown(self):
-        if len(param) == 4:
+        if len(param) == 5:
             print '\nTest: ' + param[0]
-            print 'Platform: ' + param[1]
-            print 'Browser: ' + param[2]
-            print 'Version: ' + param[3]
+            print 'URL: ' + param[1]
+            print 'Platform: ' + param[2]
+            print 'Browser: ' + param[3]
+            print 'Version: ' + param[4]
+        
         else:
             print '\nTest: ' + param[0]
-            print 'Platform: ' + param[1]
-            print 'Browser: ' + param[2]
+            print 'URL: ' + param[1]
+            print 'Platform: ' + param[2]
+            print 'Browser: ' + param[3]
         self._browser.quit()
 
 if __name__ == "__main__":
     param = []
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 5:
+        param.append(sys.argv[0])
+        param.append(sys.argv[1])
+        param.append(sys.argv[2])
+        param.append(sys.argv[3])
+        param.append(sys.argv[4])
+        del sys.argv[1:]
+    else:
         param.append(sys.argv[0])
         param.append(sys.argv[1])
         param.append(sys.argv[2])
         param.append(sys.argv[3])
         del sys.argv[1:]
-        del sys.argv[2:]
-        del sys.argv[3:]
-    else:
-        param.append(sys.argv[0])
-        param.append(sys.argv[1])
-        param.append(sys.argv[2])
-        del sys.argv[1:]
-        del sys.argv[2:]
 
     unittest.main()
